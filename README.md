c577de389be99a51b203d02ea09343e69669b2c4
F Saad <fsaad@mit.edu>
2016-08-01 10:43:51 -0400

    Support multiprocessing lovecat as well.

---
+++ README.md
@@ -0,0 +1,49 @@
+## Interactive Venturescript, Metamodeling Language, and Bayesian Query Language Environment
+
+### Installing
+The extension is contained in `iventure.py`.
+
+First add `iventure.py` to your python path. The easiest way is to create a
+symlink to the extension to `~/.ipython`:
+```
+$ cd ~/.ipython
+$ ln -s /path/to/iventure/iventure_demo.py .
+```
+Note: If all your IPython notebooks will be in this directory, you can skip
+this skip, but it is not recommended to do so.
+
+### Usage
+In the Jupyter notebook, load up extension in the first cell:
+```
+%load_ext iventure_demo
+```
+#### Open a bdb file:
+```
+%bayesdb foo.bdb
+```
+#### Write MML programs
+```
+%mml create population ...
+```
+or, for multi-line schemas.
+```
+%%mml
+drop population xyz;
+create population xyz for t (...);
+```
+#### Write BQL programs
+```
+%bql estimate mutual information of x with y within xyz;
+```
+or, for multi-line queries
+```
+%%bql
+create temp table depprobs as
+    estimate dependence probability from pairwise variables of xyz;
+.plot select * from depprobs
+```
+
+### Examples
+For a big example including complex venturescript/mml models, please visit
+[gpmcc](satellites/satellites_mml.ipynb).
+
--- iventure.py
+++ iventure.py
@@ -15,10 +15,10 @@
 #   limitations under the License.
 
 import StringIO
+import argparse
 import sys
 
 import bdbcontrib.bql_utils as bqu
-import bdbcontrib.plot_utils as bpu
 import venture.shortcuts as vs
 
 from bayeslite import BQLError
@@ -27,15 +27,24 @@ from bayeslite import bayesdb_open
 from bayeslite import bayesdb_read_csv_file
 from bayeslite import bayesdb_register_metamodel
 from bayeslite import bql_quote_name
+
 from bayeslite.core import bayesdb_get_population
 from bayeslite.core import bayesdb_has_population
+
 from bayeslite.metamodels.cgpm_metamodel import CGPM_Metamodel
+from bayeslite.metamodels.crosscat import CrosscatMetamodel
 from bayeslite.parse import bql_string_complete_p
 from bayeslite.shell.pretty import pp_list
+
 from bdbcontrib import Population
+
+from cgpm.factor.factor import FactorAnalysis
+from cgpm.kde.mvkde import MultivariateKde
+from cgpm.knn.mvknn import MultivariateKnn
 from cgpm.regressions.forest import RandomForest
 from cgpm.regressions.linreg import LinearRegression
 from cgpm.venturescript.vscgpm import VsCGpm
+from cgpm.venturescript.vsinline import InlineVsCGpm
 
 from IPython.core.error import UsageError
 from IPython.core.magic import Magics
@@ -49,8 +58,9 @@ class VentureMagics(Magics):
     def __init__(self, shell):
         super(VentureMagics, self).__init__(shell)
         self._bdb = None
+        self._path = None
         self._ripl = vs.make_ripl()
-        self._ripl.set_mode('church_prime')
+        # self._ripl.set_mode('church_prime')
         self._venturescript = []
 
     @line_cell_magic
@@ -62,19 +72,38 @@ class VentureMagics(Magics):
 
     @line_magic
     def bayesdb(self, line):
-        path = line
+        parser = argparse.ArgumentParser()
+        parser.add_argument('path', help='Path of bdb file.')
+        parser.add_argument('-j', action='store_true', help='Multiprocessing.')
+        args = parser.parse_args(line.split())
         if self._bdb is not None:
             self._bdb.close()
             self._bdb = None
-        self._bdb = bayesdb_open(path)
-        self._path = path
 
+        self._path = args.path
+        self._bdb = bayesdb_open(pathname=args.path, builtin_metamodels=False)
+
+        # Register lovecat.
+        if args.j:
+            import crosscat.MultiprocessingEngine as ccme
+            crosscat = ccme.MultiprocessingEngine(cpu_count=None)
+        else:
+            import crosscat.LocalEngine as ccle
+            crosscat = ccle.LocalEngine(seed=args.seed)
+        metamodel = CrosscatMetamodel(crosscat)
+        bayesdb_register_metamodel(self._bdb, metamodel)
+
+        # Register cgpm.
         cgpm_registry = {
-            'venturescript': self._VsCGpm,
+            'factor_analysis': FactorAnalysis,
             'linear_regression': LinearRegression,
+            'multivariate_kde': MultivariateKde,
+            'multivariate_knn': MultivariateKnn,
             'random_forest': RandomForest,
+            'venturescript': self._VsCGpm,
+            'inline_venturescript': InlineVsCGpm,
         }
-        mm = CGPM_Metamodel(cgpm_registry, multiprocess=1)
+        mm = CGPM_Metamodel(cgpm_registry, multiprocess=args.j)
         bayesdb_register_metamodel(self._bdb, mm)
 
     def _VsCGpm(self, outputs, inputs, rng, *args, **kwds):
@@ -199,24 +228,33 @@ class VentureMagics(Magics):
     # Plotting.
 
     def _cmd_bar(self, query):
+        import bdbcontrib.plot_utils as bpu
         cursor = self._bdb.execute(query)
         df = bqu.cursor_to_df(cursor)
         bpu.barplot(self._bdb, df)
 
     def _cmd_heatmap(self, query):
+        import bdbcontrib.plot_utils as bpu
         cursor = self._bdb.execute(query)
         df = bqu.cursor_to_df(cursor)
         bpu.heatmap(df)
 
     def _cmd_histogram(self, query):
+        import bdbcontrib.plot_utils as bpu
         cursor = self._bdb.execute(query)
         df = bqu.cursor_to_df(cursor)
         bpu.histogram(self._bdb, df)
 
     def _cmd_plot(self, query):
+        import bdbcontrib.plot_utils as bpu
         cursor = self._bdb.execute(query)
         df = bqu.cursor_to_df(cursor)
         bpu.pairplot(self._bdb, df)
+
+    def _cmd_scatter(self, query):
+        cursor = self._bdb.execute(query)
+        df = bqu.cursor_to_df(cursor)
+        scatter(df)
 
     _CMDS = {
         'bar': _cmd_bar,
@@ -226,8 +264,34 @@ class VentureMagics(Magics):
         'nullify': _cmd_nullify,
         'plot': _cmd_plot,
         'population': _cmd_population,
+        'scatter': _cmd_scatter,
         'table': _cmd_table,
     }
+
+def scatter(df, ax=None):
+    """Scatter the data points coloring by the classes."""
+    import matplotlib.pyplot as plt
+    import matplotlib.cm
+    import matplotlib.colors
+    if ax is None:
+        fig, ax = plt.subplots()
+    colors = 'b'
+    if df.shape[1] == 3:
+        # raise ValueError('Exactly 3 columns required for scatter.')
+        # Convert classes to integers.
+        classes = df.iloc[:,2]
+        unique = set(classes)
+        codes = {u:i for i,u in enumerate(unique)}
+        classes = [codes[c] for c in classes]
+        # Get the colors.
+        cmap = matplotlib.cm.jet
+        norm = matplotlib.colors.Normalize(
+            vmin=min(classes), vmax=max(classes))
+        mapper = matplotlib.cm.ScalarMappable(cmap=cmap, norm=norm)
+        colors = mapper.to_rgba(classes)
+    ax.scatter(df.iloc[:,0], df.iloc[:,1], color=colors)
+    ax.grid()
+    return fig
 
 def load_ipython_extension(ipython):
     ipython.register_magics(VentureMagics)
