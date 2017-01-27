ad824e5f71b30f1220c544e0c64f7c4d376821d1
F Saad <fsaad@mit.edu>
2017-01-27 08:51:30 -0500

    Expose .heatmap to iventure.

--- src/magics.py
+++ src/magics.py
@@ -384,6 +384,11 @@ class VentureMagics(Magics):
         df = utils_bql.cursor_to_df(c)
         utils_plot.clustermap(df)
 
+    def _cmd_heatmap(self, query, sql=None, **kwargs):
+        c = self._bdb.sql_execute(query) if sql else self._bdb.execute(query)
+        df = utils_bql.cursor_to_df(c)
+        utils_plot.heatmap(df)
+
     def _cmd_bar(self, query, sql=None, **kwargs):
         c = self._bdb.sql_execute(query) if sql else self._bdb.execute(query)
         df = utils_bql.cursor_to_df(c)
@@ -413,6 +418,7 @@ class VentureMagics(Magics):
     _PLTS = {
         'bar': _cmd_bar,
         'clustermap': _cmd_clustermap,
+        'heatmap' : _cmd_heatmap,
         'histogram_nominal': _cmd_histogram_nominal,
         'histogram_numerical': _cmd_histogram_numerical,
         'scatter': _cmd_scatter,
