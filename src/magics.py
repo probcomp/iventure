a3c605a7305ba4a7ce65a87d863e9f41ada17bfb
Marco Cusumano-Towner <imarcoam@gmail.com>
2017-03-06 10:41:30 -0500

    Fix code injection vulnerability

--- src/magics.py
+++ src/magics.py
@@ -438,12 +438,13 @@ class VentureMagics(Magics):
         population_id = bayesdb_get_population(self._bdb, population_name)
         table_name = bayesdb_population_table(self._bdb, population_id)
         table_name = table_name.encode('ascii','strict')
-        raw_c = self._bdb.execute(
-            '''SELECT * FROM %s;''' % (table_name,)) # TODO injection
+        qt = bql_quote_name(table_name)
+        raw_c = self._bdb.execute('SELECT * FROM %s;' % (qt,))
         raw_df = utils_bql.cursor_to_df(raw_c)
+        qt = bql_quote_name(population_name)
         depprob_c = self._bdb.execute(
             '''SELECT name0, name1, value FROM
-                (ESTIMATE DEPENDENCE PROBABILITY FROM PAIRWISE COLUMNS OF %s);''' % (population_name,)) # TODO injection
+                (ESTIMATE DEPENDENCE PROBABILITY FROM PAIRWISE COLUMNS OF %s);''' % (qt,))
         depprob_df = utils_bql.cursor_to_df(depprob_c)
         schema = self._get_schema_as_list(population_name)
         return utils_plot.interactive_depprob(raw_df, depprob_df, schema)
