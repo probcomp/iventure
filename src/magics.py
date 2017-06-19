71b602cc56451c02ffbc0963573e567283dbe65b
F Saad <fsaad@mit.edu>
2017-06-19 18:39:44 -0400

    Add --subsample to usage of .render_crosscat.

--- src/magics.py
+++ src/magics.py
@@ -614,6 +614,7 @@ class VentureMagics(Magics):
         Usage: .render_crosscat [options] <metamodel> <modelno>.
 
         Options:
+            --subsample=<n>
             --width=<w>
             --height=<c>
             --rowlabels=<colname>
